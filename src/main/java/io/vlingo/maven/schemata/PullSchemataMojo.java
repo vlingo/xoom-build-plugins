d51ec4266379f9f1c61f137c52ae712783c6bd4f
Wolfgang Werner <mail@wolfgang-werner.net>
2019-12-12 19:09:02 +0100

    Pull sources from schemata server and write them to target/generated-sources

--- src/main/java/io/vlingo/maven/schemata/PullSchemataMojo.java
+++ src/main/java/io/vlingo/maven/schemata/PullSchemataMojo.java
@@ -12,23 +12,40 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Mojo(name = "pull-schemata", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class PullSchemataMojo extends AbstractMojo {
 
-    public static final String SCHEMATA_CODE_RESOURCE_PATH = "/code";
+    public static final String SCHEMATA_CODE_RESOURCE_PATH = "/code/%s/%s";
+    public static final String SCHEMATA_REFERENCE_SEPARATOR = ":";
+    Pattern PACKAGE_NAME_PATTERN = Pattern.compile("package (.+);.*");
+
+
+    @Parameter(readonly = true, defaultValue = "${project}")
+    private MavenProject project;
 
     @Parameter(name = "schemataService")
     private SchemataService schemataService;
+
+    @Parameter(name = "outputDirectory", defaultValue = "target/generated-sources/vlingo", required = true)
+    private File outputDirectory;
 
     @Parameter(property = "schemata")
     private List<Schema> schemata;
@@ -43,9 +60,12 @@ public class PullSchemataMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException {
         logger.info(schemataService.toString());
+        this.project.addCompileSourceRoot(this.outputDirectory.toString());
         try {
             for (Schema schema : this.schemata) {
-                System.out.println(this.pullSource(schema.getRef()));
+                String reference = schema.getRef();
+                String source = this.pullSource(reference);
+                this.writeSourceFile(reference, source);
             }
         } catch (IOException e) {
             throw new MojoExecutionException("Pulling schemata failed", e);
@@ -72,7 +92,45 @@ public class PullSchemataMojo extends AbstractMojo {
         return sources.toString();
     }
 
+    private void writeSourceFile(String schemaReference, String source) throws IOException {
+
+        // we need to parse the sources as we can't access event type meta data here
+        Path sourceDirPath = packagePathFromSource(this.outputDirectory, source);
+        Files.createDirectories(sourceDirPath);
+        Path sourceFilePath = sourceDirPath.resolve(fileNameFromReference(schemaReference) + ".java");
+
+        logger.info("Writing {} to {}", schemaReference, sourceFilePath);
+        Files.write(
+                sourceFilePath,
+                source.getBytes(StandardCharsets.UTF_8),
+                StandardOpenOption.CREATE,
+                StandardOpenOption.TRUNCATE_EXISTING);
+        logger.info("Wrote {}", sourceFilePath);
+
+    }
+
+    Path packagePathFromSource(File srcRoot, String source) {
+        Matcher matcher = PACKAGE_NAME_PATTERN.matcher(source);
+        boolean isInDefaultPackage = !matcher.find();
+        if (isInDefaultPackage) {
+            return srcRoot.toPath();
+        }
+
+        String packageName = matcher.group(1);
+        String[] directories = packageName.split("\\.");
+        String relativePackagePath = String.join(File.separator, directories);
+        return srcRoot.toPath().resolve(relativePackagePath);
+    }
+
+    private String fileNameFromReference(String reference) {
+        String[] parts = reference.split(SCHEMATA_REFERENCE_SEPARATOR);
+        if (parts.length < 4) {
+            throw new IllegalArgumentException("Pass a reference in the form of <organization>:<unit>:<context>:<schema>[:<version>]");
+        }
+        return parts[3];
+    }
+
     private URL codeResourceUrl(URL baseUrl, String schemaReference, String language) throws MalformedURLException {
-        return new URL(baseUrl, String.format("/code/%s/%s", schemaReference, language));
+        return new URL(baseUrl, String.format(SCHEMATA_CODE_RESOURCE_PATH, schemaReference, language));
     }
 }
