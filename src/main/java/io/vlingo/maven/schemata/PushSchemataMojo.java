49ddeb249600b3de715adc412e839cbc581f7099
Wolfgang Werner <mail@wolfgang-werner.net>
2019-12-13 12:54:53 +0100

    Implement mojo for pushing schemata to the registry

--- src/main/java/io/vlingo/maven/schemata/PushSchemataMojo.java
+++ src/main/java/io/vlingo/maven/schemata/PushSchemataMojo.java
@@ -7,17 +7,34 @@
 
 package io.vlingo.maven.schemata;
 
+import com.google.gson.Gson;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Mojo(name = "push-schemata", defaultPhase = LifecyclePhase.INSTALL)
 public class PushSchemataMojo extends AbstractMojo {
+    public static final String SCHEMATA_VERSION_RESOURCE_PATH = "/versions/%s";
+    private final Gson gson;
+
+
+    @Parameter(readonly = true, defaultValue = "${project}")
+    private MavenProject project;
 
     @Parameter(name = "schemataService")
     private SchemataService schemataService;
@@ -31,11 +48,91 @@ public class PushSchemataMojo extends AbstractMojo {
     public PushSchemataMojo() {
         this.logger = io.vlingo.actors.Logger.basicLogger();
         logger.info("vlingo/maven: Pushing project schemata to vlingo-schemata registry.");
+        gson = new Gson();
     }
 
     @Override
     public void execute() throws MojoExecutionException {
-        logger.info(schemataService.toString());
-        logger.info(schemata.toString());
+
+        for (Schema schema : this.schemata) {
+            String reference = schema.getRef();
+            File sourceFile = schema.getSrc();
+            String description = this.generateDescription(reference, this.project.getArtifact().toString());
+
+            try {
+                String specification = new String(Files.readAllBytes(sourceFile.toPath()));
+                String previousVersion = "0.0.0"; // FIXME: configure prevVersion in publisher pom
+
+                String payload = this.payloadFrom(specification, previousVersion, description);
+                this.push(reference, payload);
+            } catch (IOException e) {
+                throw new MojoExecutionException(
+                        "Schema specification " + sourceFile.getAbsolutePath() +
+                                " could not be pushed to " + schemataService.getUrl()
+                        , e);
+            }
+        }
+
+    }
+
+    private String generateDescription(String reference, String project) {
+        StringBuilder description = new StringBuilder();
+        description.append("Schema ");
+        description.append(reference);
+        description.append(" pushed from ");
+        description.append(project);
+        description.append(" on ");
+        description.append(LocalDateTime.now().toString());
+
+        return description.toString();
+    }
+
+    private String payloadFrom(String specification, String previousVersion, String description) {
+        SchemaVersion payload = new SchemaVersion(description, specification, previousVersion);
+        return gson.toJson(payload);
+    }
+
+    private void push(String reference, String payload) throws IOException, MojoExecutionException {
+        URL schemaVersionUrl = schemaVersionUrl(this.schemataService.getUrl(), reference);
+
+        logger.info("Pushing {} to {}.", reference, schemaVersionUrl);
+
+        HttpURLConnection connection = (HttpURLConnection) schemaVersionUrl.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        OutputStream os = connection.getOutputStream();
+        os.write(payload.getBytes(StandardCharsets.UTF_8));
+        os.close();
+
+        StringBuilder sb = new StringBuilder();
+        int HttpResult = connection.getResponseCode();
+        if (HttpResult == HttpURLConnection.HTTP_CREATED) {
+            logger.info("Successfully pushed {}", schemaVersionUrl);
+        } else {
+            throw new MojoExecutionException(
+                    "Could not push " + reference
+                            + " to " + schemaVersionUrl
+                            + ": " + connection.getResponseMessage()
+                            + " - " + connection.getResponseCode());
+        }
+    }
+
+    private URL schemaVersionUrl(URL baseUrl, String schemaVersionReference) throws MalformedURLException {
+        return new URL(baseUrl, String.format(SCHEMATA_VERSION_RESOURCE_PATH, schemaVersionReference));
+    }
+
+
+    private class SchemaVersion {
+        final String description;
+        final String specification;
+        final String previousVersion;
+
+        private SchemaVersion(String description, String specification, String previousVersion) {
+            this.description = description;
+            this.specification = specification;
+            this.previousVersion = previousVersion;
+        }
     }
 }
