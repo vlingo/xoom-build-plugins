206a279c7eb7da4613644a58e226d4e61d61c51f
Danilo Ambrosio <danilo.ambrosio@gmail.com>
2021-08-06 15:11:58 -0300

    Adding service port

--- src/main/java/io/vlingo/xoom/maven/schemata/SchemataServiceDNSResolver.java
+++ src/main/java/io/vlingo/xoom/maven/schemata/SchemataServiceDNSResolver.java
@@ -8,6 +8,7 @@ package io.vlingo.xoom.maven.schemata;
 
 import org.apache.maven.model.Profile;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.StringUtils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,13 +18,23 @@ public class SchemataServiceDNSResolver {
 
   private static final String PROFILE_ID = "schemata-service";
   private static final String SERVICE_NAME = "name";
+  private static final String PORT = "port";
 
   public URL resolve(final URL actualURL,
                      final MavenProject mavenProject) throws MalformedURLException {
-    final Optional<String> serviceName =
+    final Optional<String> optionalServiceName =
             findServiceName(mavenProject);
 
-    return serviceName.isPresent() ? new URL("http", serviceName.get(), "") : actualURL;
+    if(optionalServiceName.isPresent()) {
+      final String serviceName = optionalServiceName.get();
+      final Optional<Integer> optionalPort = findPort(mavenProject);
+      if (optionalPort.isPresent()) {
+        return new URL("http", serviceName, optionalPort.get(), "");
+      }
+      return new URL("http", optionalServiceName.get(), "");
+    }
+
+    return optionalServiceName.isPresent() ? new URL("http", optionalServiceName.get(), "") : actualURL;
   }
 
   public boolean useDNS(final MavenProject mavenProject) {
@@ -31,16 +42,35 @@ public class SchemataServiceDNSResolver {
   }
 
   private Optional<String> findServiceName(final MavenProject mavenProject) {
-    final Optional<Profile> optionalProfile =
-            mavenProject.getActiveProfiles().stream()
-                    .filter(profile -> profile.getId().equals(PROFILE_ID))
-                    .findFirst();
+    final Optional<Profile> optionalProfile = findProfile(mavenProject);
 
     if(optionalProfile.isPresent() && optionalProfile.get().getProperties().containsKey(SERVICE_NAME)) {
       return Optional.of(optionalProfile.get().getProperties().getProperty(SERVICE_NAME));
     }
 
     return Optional.empty();
+  }
+
+  private Optional<Integer> findPort(final MavenProject mavenProject) {
+    final Optional<Profile> optionalProfile = findProfile(mavenProject);
+
+    if(optionalProfile.isPresent() && optionalProfile.get().getProperties().containsKey(PORT)) {
+      final String port =
+              optionalProfile.get().getProperties().getProperty(PORT);
+
+      if(!StringUtils.isNumeric(port)) {
+        throw new IllegalArgumentException("The schemata service port is not a number.");
+      }
+      return Optional.of(Integer.parseInt(port));
+    }
+
+    return Optional.empty();
+  }
+
+  private Optional<Profile> findProfile(final MavenProject mavenProject) {
+    return mavenProject.getActiveProfiles().stream()
+            .filter(profile -> profile.getId().equals(PROFILE_ID))
+            .findFirst();
   }
 
 }
